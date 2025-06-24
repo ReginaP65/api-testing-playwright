7b6353605db3e747a4de8b2d83c98d8a24f4de0d
ReginaP65 <reginai.petrova@gmail.com>
2025-06-24 16:26:30 +0300

    initial commit

---
+++ tests/dto/order-dto.ts
@@ -0,0 +1,25 @@
+export class OrderDto {
+  //define fields of the class
+  status: string
+  courierId: number
+  customerName: string
+  customerPhone: string
+  comment: string
+  id: number
+
+  constructor(
+    status: string,
+    courierId: number,
+    customerName: string,
+    customerPhone: string,
+    comment: string,
+    id: number,
+  ) {
+    this.status = status
+    this.courierId = courierId
+    this.customerName = customerName
+    this.customerPhone = customerPhone
+    this.comment = comment
+    this.id = 0
+  }
+}
--- tests/simple-solution-api.spec.ts
+++ tests/simple-solution-api.spec.ts
@@ -1,33 +1,60 @@
 import { expect, test } from '@playwright/test'
 
 import { StatusCodes } from 'http-status-codes'
+import { OrderDto } from './dto/order-dto'
+
+const baseURL = 'https://backend.tallinn-learning.ee/test-orders'
+const STATUS_OPEN = 'OPEN'
+const TEST_CUSTOMER_NAME = 'MikkTesting'
+const CUSTOMER_PHONE = '55456'
 
 test('get order with correct id should receive code 200', async ({ request }) => {
   // Build and send a GET request to the server
-  const response = await request.get('https://backend.tallinn-learning.ee/test-orders/1')
+  const apiResponse = await request.get(baseURL + '/2')
   // Log the response status, body and headers
-  console.log('response body:', await response.json())
-  console.log('response headers:', response.headers())
+  console.log('response body:', await apiResponse.json())
+  console.log('response headers:', apiResponse.headers())
   // Check if the response status is 200
-  expect(response.status()).toBe(200)
+  expect(apiResponse.status()).toBe(200)
 })
 
-test('post order with correct data should receive code 201', async ({ request }) => {
-  // prepare request body
-  const requestBody = {
-    status: 'OPEN',
-    courierId: 0,
-    customerName: 'string',
-    customerPhone: 'string',
-    comment: 'string',
-    id: 0,
-  }
+test('get order with incorrect id should receive code 400', async ({ request }) => {
+  // Build and send a GET request to the server
+  const apiResponse = await request.get(baseURL + '/11')
+  // Log the response status, body and headers
+  console.log('response body:', await apiResponse.json())
+  console.log('response headers:', apiResponse.headers())
+  // Check if the response status is 400
+  expect(apiResponse.status()).toBe(400)
+})
+
+test('post order with correct status should receive code 200', async ({ request }) => {
+  const requestBody = new OrderDto(STATUS_OPEN, 0, TEST_CUSTOMER_NAME, CUSTOMER_PHONE, 'no')
+
   // Send a POST request to the server
-  const response = await request.post('https://backend.tallinn-learning.ee/test-orders', {
+  const rawResponse = await request.post(baseURL, {
+    data: requestBody,
+  })
+  // Log the response status and body
+  console.log('response status:', rawResponse.status())
+  console.log('response body:', await rawResponse.json())
+  const responseBody = await rawResponse.json()
+  //status code
+  expect.soft(rawResponse.status()).toBe(StatusCodes.OK)
+  //assertions
+  expect.soft(responseBody.status).toBe(STATUS_OPEN)
+  expect.soft(responseBody.customerName).toBe(TEST_CUSTOMER_NAME)
+  expect.soft(responseBody.customerPhone).toBe(CUSTOMER_PHONE)
+})
+
+test('post order with incorrect status should receive code 400', async ({ request }) => {
+  const requestBody = new OrderDto('BLOCKED', 0, 'Mikk', '55456', 'no')
+  // Send a POST request to the server
+  const response = await request.post(baseURL, {
     data: requestBody,
   })
   // Log the response status and body
   console.log('response status:', response.status())
-  console.log('response body:', await response.json())
-  expect(response.status()).toBe(StatusCodes.OK)
+  console.log('response body:', await response.text())
+  expect(response.status()).toBe(StatusCodes.BAD_REQUEST)
 })
