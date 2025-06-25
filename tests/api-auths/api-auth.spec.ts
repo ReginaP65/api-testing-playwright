20adecbf7b26b3f9513a088de6c5f9a9f8d90a2d
ReginaP65 <reginai.petrova@gmail.com>
2025-06-25 14:07:46 +0300

    fix, run prettier

--- tests/api-auths/api-auth.spec.ts
+++ tests/api-auths/api-auth.spec.ts
@@ -1,4 +1,4 @@
-import {  expect, test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { LoginDto } from '../dto/loginDto'
 import { StatusCodes } from 'http-status-codes'
 
