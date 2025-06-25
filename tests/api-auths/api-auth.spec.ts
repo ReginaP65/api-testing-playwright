7f3ccc2771b627f4975ff497fdd48ec6a481f154
ReginaP65 <reginai.petrova@gmail.com>
2025-06-25 18:44:17 +0300

    fix code with prettier

--- tests/api-auths/api-auth.spec.ts
+++ tests/api-auths/api-auth.spec.ts
@@ -23,17 +23,16 @@ test('login to a student returns jwt', async ({ request }) => {
   expect.soft(response.status()).toBe(StatusCodes.OK)
   expect.soft(responseBody).toBeDefined()
 
-  const jwtRegex= /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
+  const jwtRegex = /^eyJhb[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$/
   expect.soft(responseBody).toMatch(jwtRegex)
 })
 test('login with incorrect HTTP method (GET)', async ({ request }) => {
   //const loginData = LoginDto.createLoginWithCorrectData()
-  const response = await request.get(authURL, {
-  })
+  const response = await request.get(authURL, {})
   expect.soft(response.status()).toBe(StatusCodes.METHOD_NOT_ALLOWED)
 })
 test('login with incorrect body structure', async ({ request }) => {
-  const invalidBody= LoginDto.createLoginWithIncorrectData()
+  const invalidBody = LoginDto.createLoginWithIncorrectData()
   const response = await request.post(authURL, {
     data: invalidBody,
   })
