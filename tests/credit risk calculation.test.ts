ebf739360b23964b3db73bbc8f62660ebb156d02
ReginaP65 <reginai.petrova@gmail.com>
2025-06-26 14:02:21 +0300

    Update credit risk calculation.test.ts/delete all explanation lines

--- tests/credit risk calculation.test.ts
+++ tests/credit risk calculation.test.ts
@@ -17,19 +17,15 @@ test('post order with valid income >0 should receive code 200', async ({ request
 test('post order with negative income  should receive code 400', async ({ request }) => {
   const requestBody = new OrderDto(-100, 0, 17, 1000, 12)
   const rawResponse = await request.post(baseURL, { data: requestBody })
-  //console.log('response status:', rawResponse.status())
-  //console.log('response body:', await rawResponse.json())
   expect.soft(rawResponse.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 test('post order with negative debt  should receive code 400', async ({ request }) => {
   const requestBody = new OrderDto(100, -1, 17, 1000, 12)
-  // Send a POST request to the server
   const rawResponse = await request.post(baseURL, {
     data: requestBody,
   })
   console.log('response status:', rawResponse.status())
   console.log('response body:', await rawResponse.text())
-  //status code
   expect.soft(rawResponse.status()).toBe(StatusCodes.BAD_REQUEST)
 })
 test('post order with positive debt  should receive code 200', async ({ request }) => {
